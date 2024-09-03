9c85c8278739523a03b638059f5ef1cee482f624
ellnix <ellnix@disroot.org>
2024-09-03 14:16:56 +0200

    Create a custom component

--- setting_up_react_environment/src/App.jsx
+++ setting_up_react_environment/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Greeting from './Greeting'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello World</h1>
+      <Greeting />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
