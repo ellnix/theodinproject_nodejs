d5cbac60e9d52cd15ec270b41c6d17e24ace2046
ellnix <ellnix@disroot.org>
2024-09-17 12:20:58 +0200

    Start working on CV project

---
+++ p01_cv_app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import CV from './ui/cv'
+
+function App() {
+  return (
+    <>
+      <CV />
+    </>
+  )
+}
+
+export default App
---
+++ p01_cv_app/src/ui/cv.jsx
@@ -0,0 +1,9 @@
+export default function CV() {
+  return (
+    <div className="cv-wrapper">
+      <Sidebar />
+      <MainContent />
+      <Links />
+    </div>
+  )
+}
