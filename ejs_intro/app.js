177dbbf3155b93b008571745b89292ddd3be9514
ellnix <ellnix@disroot.org>
2024-11-05 18:37:30 +0100

    Use EJS with express

---
+++ ejs_intro/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import path from "node:path";
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { message: "EJS rocks!" });
+});
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log("Listening on port " + port))
