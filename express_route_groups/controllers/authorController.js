0ec7085f8b9bc2a680ba8519144bf8242ecbf999
ellnix <ellnix@disroot.org>
2024-11-05 14:49:15 +0100

    Manually handle async errors

--- express_route_groups/controllers/authorController.js
+++ express_route_groups/controllers/authorController.js
@@ -3,12 +3,18 @@ import { get_author_by_id } from "../fake_db";
 export async function provide_author_by_id(req, res) {
   const { author_id } = req.params;
 
-  const author = await get_author_by_id(Number(author_id))
+  try {
+    const author = await get_author_by_id(Number(author_id));
 
-  if (!author) {
-    res.status(404).send('Author not found')
-    return
+    if (!author) {
+      res.status(404).send("Author not found");
+      return;
+    }
+
+    res.send(`Author name: ${author.name}`);
+  } catch (error) {
+    console.log("Failed to fetch author:");
+    console.log(error);
+    res.sendStatus(500);
   }
-
-  res.send(`Author name: ${author.name}`)
 }
