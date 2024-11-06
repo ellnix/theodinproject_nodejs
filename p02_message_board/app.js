405ff1232d62955a998b87f2ca77ccda47e579f0
ellnix <ellnix@disroot.org>
2024-11-06 11:48:30 +0100

    Allow adding a new message

--- p02_message_board/app.js
+++ p02_message_board/app.js
@@ -15,6 +15,7 @@ const messages = [
   },
 ];
 
+app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -24,6 +25,27 @@ app.get("/", (_req, res) => {
 
 app.get("/new", (_req, res) => {
   res.render("messages/new");
+});
+
+app.post("/messages", (req, res) => {
+  console.log(req);
+  const { username, message } = req.body;
+
+  if (!username) {
+    res.status(400).send("Cannot create message without username");
+    return;
+  } else if (!message) {
+    res.status(400).send("Cannot create message without a body");
+    return;
+  }
+
+  messages.push({
+    user: username,
+    text: message,
+    added: new Date(),
+  });
+
+  res.redirect("/");
 });
 
 const port = process.env.PORT || 3000;
