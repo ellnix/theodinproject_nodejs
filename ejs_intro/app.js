374ca685f34b354e75d5563de55446396b97ae32
ellnix <ellnix@disroot.org>
2024-11-06 10:40:54 +0100

    Template views with EJS

--- ejs_intro/app.js
+++ ejs_intro/app.js
@@ -2,14 +2,20 @@ import express from "express";
 import path from "node:path";
 
 const app = express();
+const links = [
+  { href: "/", text: "Home" },
+  { href: "/about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index", { message: "EJS rocks!" });
+  res.render("index", { message: "EJS rocks!", links, users });
 });
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log("Listening on port " + port))
+app.listen(port, () => console.log("Listening on port " + port));
