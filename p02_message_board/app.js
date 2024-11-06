30563857589291aecb9728fd1c0e749313dd0771
ellnix <ellnix@disroot.org>
2024-11-06 11:38:51 +0100

    Render messages

--- p02_message_board/app.js
+++ p02_message_board/app.js
@@ -2,18 +2,30 @@ import express from "express";
 import path from "node:path";
 
 const app = express();
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello world!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (_req, res) => {
-  res.render("index");
+  res.render("index", { messages });
 });
 
 app.get("/new", (_req, res) => {
   res.render("messages/new");
 });
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Listening on port ${port}...`))
+app.listen(port, () => console.log(`Listening on port ${port}...`));
