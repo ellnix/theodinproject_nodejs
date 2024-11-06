208093da0e9d4dce785a5796b80842f12297b4c2
ellnix <ellnix@disroot.org>
2024-11-06 10:54:00 +0100

    Move user view, add about route

--- ejs_intro/app.js
+++ ejs_intro/app.js
@@ -12,8 +12,15 @@ const users = ["Rose", "Cake", "Biff"];
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 app.get("/", (req, res) => {
   res.render("index", { message: "EJS rocks!", links, users });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { phone: "123456", email: "test@email", links });
 });
 
 const port = process.env.PORT || 3000;
