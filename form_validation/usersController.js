b33c2e64ad2ba8630cabda4f84165b71d0461932
ellnix <ellnix@disroot.org>
2024-11-07 09:27:49 +0100

    Add validation for user creation

--- form_validation/usersController.js
+++ form_validation/usersController.js
@@ -1,17 +1,40 @@
 import usersStorage from "./storages/usersStorage";
+import { body, validationResult } from "express-validator";
+
+const userValidation = [
+  body("first_name")
+    .trim()
+    .isAlpha()
+    .withMessage("First name must be alphanumeric")
+    .isLength({ min: 1, max: 10 })
+    .withMessage("First name must be between 1 and 10 letters"),
+  body("last_name")
+    .trim()
+    .isAlpha()
+    .withMessage("Last name must be alphanumeric")
+    .isLength({ min: 1, max: 10 })
+    .withMessage("Last name must be between 1 and 10 letters"),
+];
 
 export default {
-  index: (req, res) => {
+  index: (_req, res) => {
     res.render("users/index", { users: usersStorage.getUsers() });
   },
-  new: (req, res) => {
+  new: (_req, res) => {
     res.render("users/new");
   },
-  create: (req, res) => {
-    const { first_name, last_name } = req.body;
+  create: [
+    userValidation,
+    (req, res) => {
+      const errors = validationResult(req);
 
-    usersStorage.addUser({ first_name, last_name });
+      if (!errors.isEmpty()) {
+        return res.status(400).render("users/new", { errors: errors.array() });
+      }
 
-    res.redirect("/");
-  },
+      const { first_name, last_name } = req.body;
+      usersStorage.addUser({ first_name, last_name });
+      res.redirect("/");
+    },
+  ],
 };
