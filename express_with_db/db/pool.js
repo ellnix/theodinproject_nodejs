6cae48eb21bc76ba46117e1211a9209a50d1300c
ellnix <ellnix@disroot.org>
2024-11-07 16:42:13 +0100

    Use more environment variables

--- express_with_db/db/pool.js
+++ express_with_db/db/pool.js
@@ -2,9 +2,9 @@ import { Pool } from "pg";
 console.log(process.env.ROLE_PWD)
 
 export default new Pool({
-  host: "localhost",
+  host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER,
   database: "top_users",
   password: process.env.ROLE_PWD,
-  port: 5432,
+  port: process.env.DB_PORT || 5432,
 });
